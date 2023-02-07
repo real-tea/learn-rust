a10e4afda860e4f7ba1d80624352221f3d0ab922
real-tea <akash.0711a@gmail.com>
2023-02-07 13:08:31 +0530

    if/else loop in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("main.pdb connected");
+// use std::io;
+// use rand::Rng;
+
+fn main(){
+    for i in  0..5 {
+        if i%2==0{ println!("{} even",i);}
+        else {println!("{} odd ",i);}
+    }
 }
