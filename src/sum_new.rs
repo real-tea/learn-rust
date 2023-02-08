0a0d930f6898086a6da5d43b80a38eee08112edb
real-tea <akash.0711a@gmail.com>
2023-02-08 14:10:57 +0530

    functions

--- src/sum_new.rs
+++ src/sum_new.rs
@@ -6,8 +6,15 @@ fn sum(values : &[i32])-> i32{
     res
 }
 
+fn plus_one(x : &i32)-> i32{
+    x+1 //don't use a semicolon
+}
+
 fn main(){
     let arr = [1,2,3,4,5];
     let res = sum(&arr);
     println!("sum is : {}",res);
+    println!("after plus one function");
+    let res2 = plus_one(&res);
+    println!("{}",res2);
 }
